bc2fc7299c51dfcbe72d8df61401afaf3b16b959
euphoria <3205036516@qq.com>
2024-10-22 18:31:41 +0800

    提交修改系统名称和副标题的接口声明和领域模型

---
+++ oa-cpp/oa-c5-base/controller/system/setting/SystemSettingController.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "SystemSettingController.h"
+
+StringJsonVO::Wrapper SystemSettingController::executeModifySystemInfo(const SystemInfoDTO::Wrapper& dto)
+{
+	return {};
+}
